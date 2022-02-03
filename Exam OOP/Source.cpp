e109a54da3178bdfd88eca8e080d217217168c97
Dmytro <=>
2022-02-03 14:07:25 +0200

    Add project files.

---
+++ Exam OOP/Source.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <windows.h>
+#include <time.h>
+#include <iomanip>
+#include <fstream>
+using namespace std;
+
+
+int main() {
+    setlocale(LC_ALL, "UKR");
+
+
+    return 0;
+}
