0632820b5cf8486cd1a67fe74c612ff6509bb4c1
vadum <vadimsoluk5@gmail.com>
2022-02-04 19:36:15 +0200

    add metod user_login

--- Exam OOP/Source.cpp
+++ Exam OOP/Source.cpp
@@ -38,6 +38,34 @@ public:
         fin.close();
         return true;
     }
+
+    bool if_is_password(string& name , string& password, string& path) {
+        ifstream fin;
+        string user_n;
+        string password_c;
+
+        password += ':';
+
+        fin.open(path);
+
+        if (!fin.is_open()) {
+            cout << "ERORE!!!";
+
+        }
+        else {
+
+            while (!fin.eof()) {
+                fin >> user_n;
+                fin >> password_c;
+                if (user_n == name && password_c == password ) {
+                    return false;
+                }
+            }
+        }
+
+        fin.close();
+        return true;
+    }
 };
 
 class user : public system {
@@ -80,10 +108,57 @@ public:
             fout << name << " " << password << ":\n";
         }
         fout.close();
+
+        user_login();
+
+    }
+
+    void user_login() {
+
+        ifstream fin;
+        bool is = true;
+        bool ac;
+
+
+       
+        do {
+            line();
+            cout << "\n" << "Enter username : ";
+            cin >> name;
+            cout << "Enter password : ";
+            (cin >> password).get();
+            line();
+            cout << "\n";
+            if (!if_is_user(name, path)) {
+                if (if_is_password(name, password, path)) {
+                    cout << "Введiть правльний пароль!!!\n";
+                }
+                else {
+                    is = false;
+                }
+            }
+            else {
+                cout << "Такого користувача не iснує!!!\nСтворити акаунт???\n";
+                cout << "0 - НI\n";
+                cout << "1 - ТАК\n";
+                cout << "Зробiть свiй вибiр : ";
+                cin >> ac;
+                if (ac == true) {
+                    register_user();
+                }
+
+            }
+
+        } while (is);
+
+        
+         
     }
 
     
 };
+
+
 
 
 
@@ -92,7 +167,9 @@ int main() {
 
     user u;
 
-    u.register_user();
+    u.user_login();
+    
+    
 
     return 0;
 }
